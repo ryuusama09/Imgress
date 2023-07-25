6cf180634ccac7b84cee759c9f6067fe1fb849ca
Manan17 <shahmanan170602@gmail.com>
2023-07-26 00:37:41 +0530

    UI Changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="register" element={<Register />} />
+      <Route path="/register" element={<Register />} />
       <Route path="/upload-files/:engineID" element={<UploadFiles />} />
     </Routes>
   );
